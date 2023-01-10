e47352cc84cd80899a807d63878874e123388a7c
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2023-01-10 10:19:50 +0100

    Todo con funciones(sin clases)

--- deseoscristina/src/App.js
+++ deseoscristina/src/App.js
@@ -5,30 +5,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Component } from "react";
 
 //Crea la lista
-class DesireList extends Component {
-  render() {
-    return (
-      <ul>
-        {this.props.deseos.map((i) => (
-          <>
-            <PrintDeseo deseo={i} />
-            <Borrar deseo={i} quitar={(d) => this.props.quitar(d)} />
-          </>
-        ))}
-      </ul>
-    );
-  }
+function DesireList(props) {
+  return (
+    <ul>
+      {props.deseos.map((i) => (
+        <>
+          <PrintDeseo deseo={i} />
+          <Borrar deseo={i} quitar={(d) => props.quitar(d)} />
+        </>
+      ))}
+    </ul>
+  );
 }
 
 //Hace el formulario para introducir el deseo
-class Desire extends Component {
-  render() {
-    return (
-      <form onSubmit={this.props.onAddDeseo}>
-        <input type="text" placeholder="Escribe tu deseo" name="deseo" />
-      </form>
-    );
-  }
+function Desire(props) {
+  return (
+    <form onSubmit={props.onAddDeseo}>
+      <input type="text" placeholder="Escribe tu deseo" name="deseo" />
+    </form>
+  );
 }
 
 //Hace que cada línea sea un deseo(hay que meterlo dentro del ul)
@@ -44,41 +40,41 @@ function Borrar(props) {
   );
 }
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      deseos: ["GAMBAS", "JAMÓN"],
-    };
-  }
-
-  //Método el cual hace que se pueda añadir deseos
-  handleAniadirDeseo(event) {
-    event.preventDefault();
-    let d = this.state.deseos;
-    d.push(event.target.deseo.value);
-    this.setState({ deseos: d });
-  }
-
-  //Método el cual elimina un elemento de la lista
-  quitar(elemento) {
-    let d = this.state.deseos.filter((d) => d !== elemento);
-    this.setState({ deseos: d });
-  }
-
-  //Vista principal del programa
-  render() {
-    return (
-      <div className="App">
-        <h1> AÑADE TU REGALO FAVORITO </h1>
-        <DesireList
-          quitar={(elemento) => this.quitar(elemento)}
-          deseos={this.state.deseos}
-        />
-        <Desire onAddDeseo={this.handleAniadirDeseo.bind(this)} />
-      </div>
-    );
-  }
+function useForceUpdate() {
+  let [value, setState] = useState(true);
+  return () => setState(!value);
 }
 
+function App() {
+  let forceUpdate = useForceUpdate();
+  const [deseos, setDeseo] = useState(["GAMBAS", "JAMÓN"]);
+
+  //Método el cual hace que se pueda añadir deseos
+  const handleAniadirDeseo = (event) => {
+    event.preventDefault();
+    let d = deseos;
+    d.push(event.target.deseo.value);
+    setDeseo(d);
+    forceUpdate();//Hace que se actualice la página automáticamente
+  };
+
+  //Método el cual elimina un elemento de la lista
+  const quitar = (elemento) => {
+    let d = deseos.filter((d) => d !== elemento);
+    setDeseo(d);
+    forceUpdate();
+  };
+
+  //Vista principal del programa
+
+  return (
+    <div className="App">
+      <h1> AÑADE TU REGALO FAVORITO </h1>
+      <DesireList quitar={(elemento) => quitar(elemento)} deseos={deseos} />
+      <Desire
+        onAddDeseo={handleAniadirDeseo.bind(deseos)}
+      />
+    </div>
+  );
+}
 export default App;
